5136416a2f69c5440552a0e55363f3597eda5c6b
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2021-04-03 17:45:16 +0300

    favicon icons updated, files removed

--- sync_to_s3.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-npm run build && aws s3 sync build/ s3://luigi-medcare --profile s3admin --dryrun
-
--- todo.js
+++
@@ -1,5 +0,0 @@
-make variable color blue for text
-fix header to be responsive
-footer
-pages need finishing
-

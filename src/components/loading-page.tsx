912cd9e45a1f14fbb4cd9f23d241a6e8a89627ca
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2021-04-22 13:40:12 +0300

    new images, loading screen centered, rebuilt node-sass because node version changed

--- src/components/loading-page.tsx
+++ src/components/loading-page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import logo from "../assets/images/MedcareShipping.gif"
+import logo from "../assets/images/MedcareShipping.gif";
 
 export default function LoadingPage() {
     return (
-        <>
+        <div className="loading-page-container">
             <img src={logo} alt="medcare logo" />
-        </>
+        </div>
     );
 }
